25f74e6d5e0baba20a73dc4210c3be20b7d6f758
ricko <ricko.caesar.putra@gmail.com>
2022-12-06 23:20:06 +0700

    Initial Commit

---
+++ DynamicTableView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicTableView2
+//
+//  Created by ricko caesar putra on 06/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
